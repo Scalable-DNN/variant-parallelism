6b29d13b9ed11b6def4403c56d99eedb7df1113c
Navid Asadi <navidreza.asadi@gmail.com>
2023-02-03 17:48:54 +0100

    Update script to include cofig

--- src/training/train_models.sh
+++ src/training/train_models.sh
@@ -1,10 +1,11 @@
-export CUDA_DEVICE_ORDER="PCI_BUS_ID"
-export CUDA_VISIBLE_DEVICES=1 #,0
-export TF_XLA_FLAGS='--tf_xla_enable_xla_devices'
-# export AUTOGRAPH_VERBOSITY=0
+# export CUDA_DEVICE_ORDER="PCI_BUS_ID"
+# export CUDA_VISIBLE_DEVICES=1 #,0
+# export TF_XLA_FLAGS='--tf_xla_enable_xla_devices'
+export AUTOGRAPH_VERBOSITY=0
 #
 #
 #
 #
 # cd "."
-python multiplenets.py
+python multiplenets.py \
+  --config='train_config_ensemble_imnet.yml'
